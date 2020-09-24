55b10e469a71ff7f908f01e3a6240703e8fac359
clouds-wd <61484036+clouds-wd@users.noreply.github.com>
2020-09-24 13:02:24 +0800

    Create Union_Find.py

---
+++ Python/Union_Find.py
@@ -0,0 +1,34 @@
+class UnionFindSet:
+    def __init__(self, n):
+        self._parents = [i for i in range(n + 1)]
+        self._ranks = [1 for i in range(n + 1)]
+
+    def find(self, u):
+        while u != self._parents[u]:
+            self._parents[u] = self._parents[self._parents[u]]
+            u = self._parents[u]
+        return u
+
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv:
+            return False
+
+        if self._ranks[pu] < self._ranks[pv]:
+            self._parents[pu] = pv
+        elif self._ranks[pu] > self._ranks[pv]:
+            self._parents[pv] = pu
+        else:
+            self._parents[pv] = pu
+            self._ranks[pu] += 1
+
+    def display(self):
+        print(self._parents)
+        print(self._ranks)
+
+        return True
+
+
+if __name__ == '__main__':
+    tem = UnionFindSet(10)
+    tem.display()
