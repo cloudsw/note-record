a409daecad8df5da2cc13045a4315ba6ef6b7b39
clouds-wd <61484036+clouds-wd@users.noreply.github.com>
2020-10-17 09:29:06 +0800

    Create Linked list and tree.py

---
+++ Python/Linked list and tree.py
@@ -0,0 +1,26 @@
+class linked_node:
+    def __init__(self, target):     # 在python中用类表示链表
+        self.data = target
+        self.next = None
+
+class tree:
+    def __init__(self, root):       # 在python中二叉树的表示方法
+        self.root = root
+        self.lnext = None
+        self.rnext = None
+
+
+def show(tem):
+    while tem:
+        print(tem.data, end=' ')
+        tem = tem.next
+
+
+if __name__ == "__main__":
+    a = linked_node(1)
+    b = linked_node(2)
+    c = linked_node(3)
+    a.next = b
+    b.next = c
+
+    show(a)
