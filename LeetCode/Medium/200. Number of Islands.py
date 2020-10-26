63398f21b74bd3e5e085e6960c9c4a4464002ee6
clouds-wd <61484036+clouds-wd@users.noreply.github.com>
2020-10-26 10:30:10 +0800

    Create 200. Number of Islands.py

---
+++ LeetCode/Medium/200. Number of Islands.py
@@ -0,0 +1,29 @@
+# 图
+
+
+def digui(grid, x, y, n, m):
+    if x < 0 or y < 0 or x >=n or y >= m or grid[y][x] == '0':
+        return
+    else:
+        grid[y][x] = '0'
+        digui(grid, x + 1, y, n, m)     # 使用递归，继续访问此节点的周围节点（子节点） 上，下，左，右
+        digui(grid, x - 1, y, n, m)
+        digui(grid, x, y + 1, n, m)
+        digui(grid, x, y - 1, n, m)
+
+
+def Number_of_Islands(grid):
+    m = len(grid)
+    if m == 0:
+        return 0
+    n = len(grid[0])
+    ans = 0
+    for y in range(m):
+        for x in range(n):
+            if grid[y][x] == '1':
+                ans += 1
+                digui(grid, x, y, n, m)
+    return ans
+
+
+if __name__=='__main__':
