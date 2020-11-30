6be59c68931f600b38095df06497a824ba5fcead
clouds-wd <61484036+clouds-wd@users.noreply.github.com>
2020-11-30 15:10:34 +0800

    Update 684. Redundant Connection.py

--- LeetCode/Medium/684. Redundant Connection.py
+++ LeetCode/Medium/684. Redundant Connection.py
@@ -1,21 +1,23 @@
-def judge_node(parent, tem):
-    while tem != parent[tem]:
-        tem = parent[tem]
-    return tem
+class Redundant_Connection:     # 采用并查集来判断是否存在环
+    def __init__(self, vertex, count):
+        self.vertex = vertex
+        self.parent = [i for i in range(count + 1)]
 
+    def find(self, tem):
+        while tem != self.parent[tem]:
+            tem = self.parent[tem]
+        return tem
 
-def Redundant_Connection(node, count):
-    parent = [i for i in range(count + 1)]
-    for ver in node:
-        node_1 = judge_node(parent, ver[0])
-        node_2 = judge_node(parent, ver[1])
-        if node_1 == node_2:
-            return ver
-        else:
-            parent[ver[1]] = ver[0]
+    def union(self):
+        for ver in self.vertex:
+            node_1, node_2 = self.find(ver[0]), self.find(ver[1])
+            if node_1 == node_2:
+                return ver
+            else:
+                self.parent[ver[1]] = ver[0]
 
 
 if __name__ == '__main__':
     node = [[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]]
     res = Redundant_Connection(node, 5)
-    print(res)
+    print(res.union())
