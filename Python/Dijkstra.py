4a33dd5cacedfc4a7a00494019b64c397c75628f
cloudsw <61484036+cloudsw@users.noreply.github.com>
2021-07-07 15:30:55 +0800

    Update Dijkstra.py

--- Python/Dijkstra.py
+++ Python/Dijkstra.py
@@ -54,6 +54,6 @@ if __name__ == '__main__':
     # print(parent)
     # print(distance)
     print("路径为：", end="")
-    print("test")
+    print("GitHub")
     for i in range(len(list1)):
         print(list1.pop(), end=" ")
